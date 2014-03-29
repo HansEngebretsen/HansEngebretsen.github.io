b9ef14a64cbc7d6121a9342e4d842769747b62ad
Hans Engebretsen <engebretsenh@gmail.com>
2014-03-29 11:01:21 -0500

    Added tags, fixed a few issues with project container extending when no project is active

--- scripts/main.js
+++ scripts/main.js
@@ -16,10 +16,13 @@ $( document ).ready(function() {
 $('.preview-image, .project-container .more').on('click', function(){
       $('html,body').animate({scrollTop: $('.project-container').offset().top - '10'}, 400);
       $(this).parents('li').addClass('active');
+      $(this).parents('ul').addClass('project-active');
 });
 $('.project-container .back').click(function(e) {
   e.preventDefault();
   $(this).parents('li').removeClass('active');
+  $(this).parents('ul').removeClass('project-active');
+
 });
 
 // Navigation
