62a672c8ae84d4792fa56e2ca90068ee69696fe5
Hans Engebretsen <hans@Hanss-MacBook-Pro-2.local>
2015-11-10 22:39:43 -0600

    updated jekyll version and bugs

--- Gruntfile.js
+++ Gruntfile.js
@@ -110,5 +110,5 @@ module.exports = function(grunt) {
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
     grunt.registerTask("serve", ["shell:jekyllServe"]);
-    grunt.registerTask('default', ['watch'], ["shell:jekyllServe"]);
+    grunt.registerTask('default');
 };
